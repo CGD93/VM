01c70f8461e56b7f5e09e16ae7e5d0c97f554bf0
ChoiGwangDeok <111724209+CGD93@users.noreply.github.com>
2023-05-24 10:25:17 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,14 @@ variable "vm_name" {}
 variable "computer_name" {}
 variable "user_name" {}
 variable "user_password" {}
+
+variable "NSG-name" {}
+variable "security_rule_name" {}
+variable "security_rule_priority" {}
+variable "security_rule_direction" {}
+variable "security_rule_access" {}
+variable "security_rule_protocol" {}
+variable "security_rule_source_port_range" {}
+variable "security_rule_destination_port_range" {}
+variable "security_rule_source_address_prefix" {}
+variable "security_rule_destination_address_prefix" {}
